9dd8ff43c9cb19a40a9eb5b827464b8d4581e6a2
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2022-10-12 00:33:56 +0330

    Initial Entry

---
+++ Number Guessing Game/Main.py
@@ -0,0 +1,21 @@
+# Thanks to: Aman Kharwal
+# Source Link: https://TheCleverProgrammer.com/2022/06/29/number-guessing-game-using-python
+
+import random
+
+random_number = random.randrange(1,10)
+print("What's your guess number between 1 to 10?")
+
+def main():
+    while(True):
+        guessed_numbers = int(input("Please enter a number: "))
+        if guessed_numbers > random_number:
+            print("It was high!")
+        elif guessed_numbers < random_number:
+            print("It was low!")
+        else:
+            print("Congratulations, It was right.")
+            break            
+
+if __name__ == "__main__":
+    main()
