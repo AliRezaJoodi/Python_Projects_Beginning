86766460ec07b96aed862a091ba6d1fb4180d85c
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2022-10-13 22:28:29 +0330

    Initial Entry

---
+++ Story Generator/README.md
@@ -0,0 +1,9 @@
+It's about **Story Generator**.
+
+### Assignment
+Our task is to generate a random story every time the user runs the program. I will first store the parts of the stories in different lists, then the Random module can be used to select the random parts of the story stored in different lists
+
+Thanks to: [Aman Kharwal](https://thecleverprogrammer.com/about)  
+Source Link:  [Story Generator with Python](https://thecleverprogrammer.com/2021/01/11/story-generator-with-python)
+
+**Note**: [You can go here to download a single folder or file from GitHub.com](https://minhaskamal.github.io/DownGit/#/home)
---
+++ Story Generator/main.py
@@ -0,0 +1,19 @@
+# https://thecleverprogrammer.com/2021/01/11/story-generator-with-python/
+# https://github.com/amankharwal/21-python-mini-projects/blob/main/story.py
+
+import random
+
+when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago','On 20th Jan']
+who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat']
+name = ['Ali', 'Miriam','daniel', 'Hoouk', 'Starwalker']
+residence = ['Barcelona','India', 'Germany', 'Venice', 'England']
+went = ['cinema', 'university','seminar', 'school', 'laundry']
+happened = ['made a lot of friends','Eats a burger', 'found a secret key', 'solved a mistery', 'wrote a book']
+
+print( \
+    random.choice(when) + ', ' + \
+    random.choice(name) + ' that lived in ' + \
+    random.choice(residence) + ', went to the ' + \
+    random.choice(went) + ' and ' + \
+    random.choice(happened)\
+)
