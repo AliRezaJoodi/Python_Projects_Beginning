5067f102d109d64e8d257cfcb794d71bdb03905b
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2022-10-13 19:24:48 +0330

    Update

--- Find Missing Number/Find_Missing_Number.py
+++ Find Missing Number/Find_Missing_Number.py
@@ -6,8 +6,8 @@ def find_missing_number(numbers, dibag=False):
     if dibag==True: print("Sort List:      ",numbers)
     max_number=numbers[len(numbers)-1]
     if dibag==True: print("max_number:     ",max_number)
-    missing_number=[]
+    missing_numbers=[]
     for i in range(1,max_number+1):
-        if i not in numbers: missing_number.append(i)
-    if dibag==True: print("missing_numbers: ", missing_number)
-    return missing_number
+        if i not in numbers: missing_numbers.append(i)
+    if dibag==True: print("missing_numbers: ", missing_numbers)
+    return missing_numbers
