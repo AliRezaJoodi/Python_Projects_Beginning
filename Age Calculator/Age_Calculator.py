1544f6501cbc7a160316e86379d93b57a55c884a
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2022-10-16 03:55:03 +0330

    Update

--- Age Calculator/Age_Calculator.py
+++ Age Calculator/Age_Calculator.py
@@ -9,17 +9,20 @@ def age_calculator_v10(birth_date):
     dispaly ("\n", "", debug)
     dispaly ("Function:", "age_calculator_v10", debug)
     
+    age=0
     today_date = date.today()
     dispaly ("Today Date:", today_date, debug)
     dispaly ("Birth Date:", birth_date, debug)
     
-    age = (today_date - birth_date).days
-    dispaly ("Age (Days):", age, debug)
-    age=int(age/365.25)
-    dispaly ("Age (Years):", age, debug)
-    
+    if today_date > birth_date:
+        age = (today_date - birth_date).days
+        dispaly ("Age (Days):", age, debug)
+        age=int(age/365.25)
+        dispaly ("Age (Years):", age, debug)
+    else:
+        dispaly ("Note:", "Birth Date Was Invalid", debug)
     return age
 
 if __name__ == "__main__":
     debug = True
-    age_calculator_v10(date(1984, 12, 30))
+    age_calculator_v10(date(1984, 7, 1))
--- Age Calculator/Main.py
+++ Age Calculator/Main.py
@@ -6,11 +6,11 @@ from Dispaly import dispaly
 from datetime import date
 
 def main():
-    year_int=   int(input("Enter Year of Your Birth:  "))
-    month_int=  int(input("Enter Month of Your Birth: "))
-    day_int=    int(input("Enter Day of Your Birth:   "))
-    birth_date= date(year_int, month_int, day_int)
-    age=        age_calculator_v10(birth_date)
+    year_int =   int(input("Enter Year of Your Birth:  "))
+    month_int =  int(input("Enter Month of Your Birth: "))
+    day_int =    int(input("Enter Day of Your Birth:   "))
+    birth_date = date(year_int, month_int, day_int)
+    age = age_calculator_v10(birth_date)
     dispaly ("Age:", age)
 
 if __name__ == "__main__":
